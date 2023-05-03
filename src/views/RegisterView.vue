04f3bcbc33169122fce2df392ea831ec695984aa
Bram-Colleman <r0820751@student.thomasmore.be>
2023-05-03 15:45:10 +0200

    feat: add register html and remove unused files

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,39 @@
+<script setup>
+</script>
+
+<template>
+<h1>Register</h1>
+<form @submit.prevent="">
+    <label for="givenName">Voornaam</label>
+    <input type="text" name="givenName">
+    <label for="familyName">Achternaam</label>
+    <input type="text" name="familyName">
+    <label for="dateOfBirth">Geboortedatum</label>
+    <input type="date" name="dateOfBirth">
+    <label for="gender">Geslacht</label>
+    <div>
+        <input type="radio" name="gender" value="M" id="male"> <label for="male">Man</label>
+    </div>
+    <div>
+        <input type="radio" name="gender" value="F" id="female"> <label for="female">Vrouw</label>
+    </div>
+    <div>
+        <input type="radio" name="gender" value="X" id="other"> <label for="other">Anders</label>
+    </div>
+    <label for="password">Wachtwoord</label>
+    <input type="password" name="password">
+    <input type="password" name="password">
+    <label for="email">e-mail</label>
+    <input type="email" name="email">
+    <input type="submit" name="">
+</form>
+</template>
+
+<style scoped>
+form {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    width: 15rem;
+}
+</style>
