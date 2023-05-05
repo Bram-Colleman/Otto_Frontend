67b3bdb9f6893d461a5538eb9a260292f466d609
Bram-Colleman <r0820751@student.thomasmore.be>
2023-05-05 13:00:12 +0200

    safety push

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,17 +11,9 @@
 <style scoped>
 .nav {
     position: fixed;
-    bottom: 500px;
-    left: 0;
 }
 img {
     background-color: #3289F3;
-    position: absolute;
-    z-index: 1;
-    top: 2rem;
     margin: 0  auto;
-    border-radius: 50%;
-    outline: 2px white solid;
-    outline-offset: -1px;
 }
 </style>
