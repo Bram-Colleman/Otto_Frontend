33933165e542cc7d925f615cedff5e9501a0273f
Senne Christiaens <senne.christiaens@icloud.com>
2023-05-08 18:54:56 +0200

    feat: add calendar to calendarview

--- src/views/CalenderView.vue
+++ src/views/CalenderView.vue
@@ -1,15 +1,14 @@
 <script setup>
-import Navigation from '../components/Navigation.vue';
-
+import Navigation from "../components/Navigation.vue";
+import DatePicker from "../components/Calendar.vue";
 </script>
 
 <template>
   <Navigation v-bind:active="'calender'"></Navigation>
   <div class="card">
     <h1>Beschikbaarheden</h1>
+    <DatePicker />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
