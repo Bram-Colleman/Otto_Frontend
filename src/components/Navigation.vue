88f4aa7a6587e66c2e79657995917615dfc66866
Bram Colleman <bram.colleman@telenet.be>
2023-05-08 19:48:58 +0200

    feat: add map to home

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -38,6 +38,7 @@ onMounted(() => {
     background: white;
     box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.25);
     flex-grow: 1;
+    z-index: 100;
 }
 img {
     margin-top: 16px;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Navigation from '../components/Navigation.vue';
+import Map from '../components/leaflet/Map.vue';
 
 
 
@@ -7,12 +8,19 @@ let name = localStorage.getItem('name').split(" ")[0];
 </script>
 
 <template>
-  <Navigation v-bind:active="'home'"></Navigation>
+  <Navigation v-bind:active="'home'" style="z-index: 1000;"></Navigation>
   <div class="card">
     <h1>Hi, {{name}} 👋</h1>
+    <Map class="map"></Map>
   </div>
 </template>
 
 <style scoped>
-
+.card {
+  padding-left: 0!important;
+  padding-right: 0!important;
+}
+.map {
+  border-radius: 10px;
+}
 </style>
