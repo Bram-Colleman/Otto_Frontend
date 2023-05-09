fb888f7059b8fc94524f8a9b434a1af8c3eacd28
Bram Colleman <bram.colleman@telenet.be>
2023-05-09 17:33:07 +0200

    feat: add styling to route page

--- src/views/RouteView.vue
+++ src/views/RouteView.vue
@@ -20,39 +20,57 @@ onMounted(() => {
     .then((response) => response.json())
     .then((data) => {
       rides.value = data.rides;
-      console.log(data.rides);
     });
 });
 moment.locale("es");
 moment().tz("America/Los_Angeles").format();
+console.log(rides.value[0]);
 </script>
 
 <template>
   <Navigation v-bind:active="'route'"></Navigation>
   <div class="card">
     <h1>Routes</h1>
-    <div v-for="ride in rides" :key="rides.id">
-      <div class="background">
-        <div class="clientinfo">
-          <div class="flextime">
-            <img src="../assets/icons/clock.svg" alt="clock" />
-            <span>{{ moment(ride.timeStamp).format("DD MMMM YYYY HH:mm") }}</span>
+    <div v-if="rides[0]">
+      <span><strong>Mijn routes</strong></span>
+      <div v-for="ride in rides" :key="rides.id">
+        <div class="background">
+          <div class="clientinfo">
+            <div class="flextime">
+              <img src="../assets/icons/clock.svg" alt="clock" />
+              <span>{{
+                moment(ride.timeStamp).format("DD MMMM YYYY HH:mm")
+              }}</span>
+            </div>
+            <div class="flexadress">
+              <img src="../assets/icons/ping.svg" alt="location" />
+              <span>{{ ride.startlat }}</span>
+            </div>
           </div>
-          <div class="flexadress">
-            <img src="../assets/icons/ping.svg" alt="location" />
-            <span>{{ ride.startlat }}</span>
+          <div class="rideicons">
+            <img id="target" src="../assets/icons/target.svg" alt="target" />
+            <img src="../assets/icons/chatblue.svg" alt="chat" />
           </div>
-        </div>
-        <div class="rideicons">
-          <img id="target" src="../assets/icons/target.svg" alt="target" />
-          <img src="../assets/icons/chatblue.svg" alt="chat" />
         </div>
       </div>
+    </div>
+
+    <div v-if="rides[0] === undefined" class="msg">
+      <span>You don't have any rides</span>
     </div>
   </div>
 </template>
 
 <style scoped>
+.card {
+  margin-bottom: 8rem;
+}
+.msg {
+  height: 3rem;
+  background-color: white;
+  text-align: center;
+  font-weight: bold;
+}
 
 .rideicons img {
   width: 1.5rem;
@@ -82,7 +100,7 @@ moment().tz("America/Los_Angeles").format();
   background: #ffffff;
   box-shadow: 0px 4px 44px rgba(0, 0, 0, 0.06);
   border-radius: 5px;
-  margin-top: 1.5rem;
+  margin: 0.75rem 0;
   padding: 1.5rem 1.5rem;
 }
 </style>
