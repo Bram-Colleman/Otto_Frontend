7a73ed4d76016d31903410003b9ecfc9deb62222
Senne Christiaens <senne.christiaens@icloud.com>
2023-05-09 17:04:49 +0200

    feat: add rides from database to routeview

--- src/views/RouteView.vue
+++ src/views/RouteView.vue
@@ -1,21 +1,88 @@
 <script setup>
-import Navigation from '../components/Navigation.vue';
-import Mapbox from '../components/leaflet/Map.vue';
+import Navigation from "../components/Navigation.vue";
+import { onMounted, ref } from "vue";
+import moment from "moment-timezone";
 
+let rides = ref([]);
+
+//fetch rides by driver and display them
+
+onMounted(() => {
+  //  fetch("https://otto-backend.onrender.com/api/ride/getbydriver"
+  let apiUrl = "http://localhost:3000/api/ride/getbydriver";
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      rides.value = data.rides;
+      console.log(data.rides);
+    });
+});
+moment.locale("es");
+moment().tz("America/Los_Angeles").format();
 </script>
 
 <template>
   <Navigation v-bind:active="'route'"></Navigation>
   <div class="card">
     <h1>Routes</h1>
-    <Mapbox class="map"></Mapbox>
-
+    <div v-for="ride in rides" :key="rides.id">
+      <div class="background">
+        <div class="clientinfo">
+          <div class="flextime">
+            <img src="../assets/icons/clock.svg" alt="clock" />
+            <span>{{ moment(ride.timeStamp).format("DD MMMM YYYY HH:mm") }}</span>
+          </div>
+          <div class="flexadress">
+            <img src="../assets/icons/ping.svg" alt="location" />
+            <span>{{ ride.startlat }}</span>
+          </div>
+        </div>
+        <div class="rideicons">
+          <img id="target" src="../assets/icons/target.svg" alt="target" />
+          <img src="../assets/icons/chatblue.svg" alt="chat" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.card {
-  padding-left: 0!important;
-  padding-right: 0!important;
+
+.rideicons img {
+  width: 1.5rem;
+}
+
+#target {
+  padding-right: 1.5rem;
+}
+
+.flextime,
+.flexadress {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+}
+
+.flextime img,
+.flexadress img {
+  margin-bottom: 0.2rem;
+  margin-top: 0.2rem;
+  margin-right: 0.5rem;
+}
+.background {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #ffffff;
+  box-shadow: 0px 4px 44px rgba(0, 0, 0, 0.06);
+  border-radius: 5px;
+  margin-top: 1.5rem;
+  padding: 1.5rem 1.5rem;
 }
 </style>
